7b01c99906970361a2e6bb37cac79e1e1fc20f55
javeriaahmad73 <javeriaahmad1237@gmail.com>
2026-04-02 02:59:56 -0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
 import settings
 
 def height_perct(percentage):
-    return (settings.height/100)*percentage
+    return (settings.height/100)*percentage # Return  the calculated height percentage based on settings.height
 
 def width_perct(percentage):
     return (settings.width/100)*percentage
