f34386e44d8eacadeec0d657591eca9425b65463
javeriaahmad73 <javeriaahmad1237@gmail.com>
2026-04-02 02:52:28 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-width=1000
-height=720
+width=1000 #screen width in pixels for the Minesweeper game window
+height=720 #screen height in pixels for Mineaweeper gamewindow
 GRID_SIZE=6
 CELL_COUNT=GRID_SIZE**2
 MINES_COUNT=(CELL_COUNT)//4
