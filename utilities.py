626abd01d8a90f825cf45e8ff3f538cf0222d1a7
javeriaahmad73 <javeriaahmad1237@gmail.com>
2026-04-02 03:03:52 -0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -4,5 +4,5 @@ def height_perct(percentage): # define a function to calculate a percentage of s
     return (settings.height/100)*percentage # Return  the calculated height percentage based on settings.height
 
 def width_perct(percentage):  # define a function to calculate a percentage of screen width
-    return (settings.width/100)*percentage
+    return (settings.width/100)*percentage # Return  the calculated width percentage based on settings.width
 
